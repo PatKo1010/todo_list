5466171325f8aa334fe2e16545047daf46c76df5
PatKo1010 <Pat19951010>
2021-09-17 16:15:16 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require ('mongoose')
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on ('error', ()=> {
+  console.log (`mongo-db error`)
+})
+db.once ('open', ()=> {
+  console.log (`mongodb connected`)
+})
 
 app.get ('/', (req,res) => {
   res.send (`Hello world`)
